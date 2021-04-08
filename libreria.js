35fc3ac9d284a2640d575f6dab193bf21dfa6648
wild.chamo <joselitobedoya230@gmail.com>
2021-04-08 14:24:34 -0500

    agrega los textos

--- libreria.js
+++ libreria.js
@@ -20,6 +20,9 @@ function leerDatos() {
 	datos["nombre"] = document.getElementById("nombre").value;
 	datos["costo"] = document.getElementById("costo").value;
 	datos["iva"] = document.getElementById("iva").value;
+	datos["estado"] = document.getElementsByClassName("estado").value;
+    datos["fecha"] = document.getElementById("fecha").value;
+    datos["comentarios"] = document.getElementById("comentarios").value;
 	return datos;
 	}
 
@@ -34,14 +37,23 @@ function insertar(datos) {
 	campo2 = NuevoReg.insertCell(1);
 	campo2.innerHTML = datos.nombre;
 
-	campo1 = NuevoReg.insertCell(2);
-	campo1.innerHTML = datos.costo;
-	
-	campo2 = NuevoReg.insertCell(3);
-	campo2.innerHTML = datos.iva;
+	campo3 = NuevoReg.insertCell(2);
+	campo3.innerHTML = datos.costo;
 
-    campo3 = NuevoReg.insertCell(4);
-	campo3.innerHTML = "<a onClick='onClick_Editar(this);'>Editar</a> <a  onClick='onClick_Borrar(this);'>Borrar</a>";
+    campo4 = NuevoReg.insertCell(3);
+	campo4.innerHTML = datos.iva;
+
+    campo5 = NuevoReg.insertCell(4);
+	campo5.innerHTML = datos.estado;
+
+    campo6 = NuevoReg.insertCell(5);
+	campo6.innerHTML = datos.fecha;
+
+    campo7 = NuevoReg.insertCell(6);
+	campo7.innerHTML = datos.comentarios;
+
+    campo8 = NuevoReg.insertCell(7);
+	campo8.innerHTML = "<a onClick='onClick_Editar(this);'>Editar</a> <a  onClick='onClick_Borrar(this);'>Borrar</a>";	
 }
 
 function actualizar( datos ) {
@@ -49,6 +61,9 @@ function actualizar( datos ) {
 	regSeleccionado.cells[1].innerHTML = datos.nombre;
 	regSeleccionado.cells[2].innerHTML = datos.costo;
 	regSeleccionado.cells[3].innerHTML = datos.iva;
+	regSeleccionado.cells[4].innerHTML = datos.estado;
+	regSeleccionado.cells[5].innerHTML = datos.fecha;
+	regSeleccionado.cells[6].innerHTML = datos.comentarios;
 }
 
 function onClick_Editar( reg ) {
